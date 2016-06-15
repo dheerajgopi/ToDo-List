f5e63b86965f04b96239cbcfbd22e27b3a500758
Dheeraj Gopinath <dheerajgopinath@gmail.com>
2016-06-15 13:17:38 +0530

    add router using gorilla/mux

--- server.go
+++ server.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "The url path is %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true) // redirects trailing slash
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "The url path is %q", html.EscapeString(r.URL.Path))
 }
